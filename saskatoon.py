c7bdd7686e5491367ed85ea02425497e08ebf5dd
Ezra Lalonde <ezrais@gmail.com>
2014-08-31 20:24:49 -0600

    Create saskatoon.py

---
+++ saskatoon.py
@@ -0,0 +1,20 @@
+import getpass
+import sys
+from splinter import Browser 
+                    
+with Browser() as browser: 
+    # Visit URL 
+    url = 'https://apps2.saskatoon.ca/lapp/eUtilAcct/AccountSummary.aspx'
+    browser.visit(url) 
+
+    username = browser.find_by_xpath('//input[contains(@name, "Username")]')[0]
+    username.fill(sys.argv[1])
+
+    password = browser.find_by_xpath('//input[contains(@name, "Password")]')[0]
+    password.fill(getpass.getpass())
+
+    browser.find_by_xpath('//input[contains(@name, "Submit")]').first.click()
+
+    figures = browser.find_by_xpath('//span[contains(text(), "$")]')
+    for figure in figures:
+        print figure.value
